82ac128c27d5de6a07b183e4dd7031086c20c44e
Nate Fatkhiyev <nfatkhiyev@gmail.com>
2020-09-15 11:41:42 -0400

    try serialport

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,12 @@ fn main() {
         flow_control: FlowControl::None,
         parity: Parity::None,
         stop_bits: StopBits::One,
-        timeout: time::Duration::from_millis(10),
+        timeout: time::Duration::from_secs(1),
     };
     let button_reader =
         serialport::open_with_settings("/dev/ttyACM0", &button_reader_settings).unwrap();
+
+    let clone = button_reader.try_clone().unwrap();
     let mut buf_reader = BufReader::new(button_reader);
     loop {
         let mut msg = String::new();
@@ -20,6 +22,7 @@ fn main() {
         match buf_reader.read_line(&mut msg) {
             Ok(_) => {
                 println!("{:?}", msg);
+                clone.clear(serialport::ClearBuffer::All).unwrap();
             }
 
             Err(ref e) if e.kind() == ErrorKind::TimedOut => (),
